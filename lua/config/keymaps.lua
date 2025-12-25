5c0f8a6333b4155a47880efbfe4ed5a2be57ad30
WeiShx <2642138150@qq.com>
2025-12-26 02:09:54 +0800

    恢复flash.nvim的keymaps，保留s和S在normal mode的功能

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,5 +2,7 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
-vim.keymap.del("n", "<S-h>")
-vim.keymap.del("n", "<S-l>")
+vim.keymap.del("n", "H")
+vim.keymap.del("n", "L")
+vim.keymap.del("n", "s")
+vim.keymap.del("n", "S")
